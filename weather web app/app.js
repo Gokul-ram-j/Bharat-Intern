5878893364230300f48a82ec5cd085d7363bc80f
gokul-ram-j <gokulram221@gmail.com>
2024-01-25 22:40:28 +0530

    security purpose

--- weather web app/app.js
+++ weather web app/app.js
@@ -1,11 +1,11 @@
 
-fetch("api.openweathermap.org/data/2.5/forecast?q={chennai}&appid={c9368f9dd49c8c5fecb45b95d3b685aa}")
+fetch("api.openweathermap.org/data/2.5/forecast?q={chennai}&appid={}")
 .then((res)=>res.json)
 .then((data)=>{
     console.log(data)
 })
 
-fetch("api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid={c9368f9dd49c8c5fecb45b95d3b685aa}")
+fetch("api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid={}")
 .then((res)=>res.json)
 .then((data)=>{
     console.log(data)
